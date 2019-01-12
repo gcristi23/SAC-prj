97c7a1f6422fbd47ba90740f1deb6a34ac468d60
Cristi Grama <g.cristi23@gmail.com>
2019-01-12 16:42:41 +0200

    Updated

--- games.py
+++ games.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, request, session
-import os, json
-from flask_sqlalchemy import SQLAlchemy, BaseQuery, _QueryProperty
-from models import Base, User, Game
+import json
+import os
+
 import numpy as np
+import requests as rq
+from flask import Flask, render_template, request, session
+from flask_sqlalchemy import SQLAlchemy, BaseQuery, _QueryProperty
+
+from models import Base, User, Game
 
 app = Flask(__name__)
 
@@ -27,11 +31,16 @@ db.session.expunge_all()
 
 @app.route('/')
 def index():
+	global logged_user
 	if not session.get('logged_in'):
 		return render_template('login.html')
 	else:
+		games_owned = []
+		user = db.session.query(User).filter(User.email == logged_user.email).first()
+		for game in user.games:
+			games_owned.append(game.name)
 		html_page = render_template("front.html", **{
-				'games': games,
+			'games': list(set(games) - set(games_owned)),
 				'logged_user': logged_user
 		})
 
@@ -47,15 +56,15 @@ def select():
 		return game_selection_error()
 
 	user = logged_user
-	game = Game(name=game_name, user=user)
+	game = db.session.query(Game).filter(Game.name == game_name).first()
 	user.games.append(game)
 
 	try:
-		db.session.add(game)
+		db.session.commit()
 	except Exception as e:
 		print(e)
 	
-	db.session.commit()
+
 
 	return my_games()
 
@@ -68,30 +77,93 @@ def my_games():
 	global logged_user
 
 	user = db.session.query(User).filter(User.email == logged_user.email).first()
-
-	game_recs = []
+	games_owned = []
+	game_recs = None
 	for game in user.games:
 		k = np.max(costs[top_10k[game.name][0]])
 		the_id = np.where(costs[top_10k[game.name][0]] == k)
 
 		rez = []
-		print(len(the_id[0]))
 		for k in the_id[0]:
 			if len(rez) >= 5:
 				break
 			for x in games:
 				if top_10k[x][0] == k:
 					rez.append(x)
+		games_owned.append(game.name)
+		if game_recs is None:
+			game_recs = set(rez)
+		else:
+			game_recs = game_recs | set(rez)
+	if game_recs is None:
+		game_recs = []
+	else:
+		game_recs = list(game_recs - set(games_owned))
+	with app.test_request_context():
+		html_page = render_template("my_games.html", **{
+			'user': user.name,
+				'games': game_recs
+		})
+		return html_page
 
-		game_recs.append((game.name, rez))
+
+@app.route('/steam', methods=['POST'])
+def steam():
+	global logged_user
+
+	steam = request.form['steam']
+
+	data = rq.get("https://steamcommunity.com/id/%s/?xml=1" % steam)
+	logged_user = db.session.query(User).filter(User.name == steam).first()
+	try:
+		id64 = data.text.split("ID64>")[1].split("<")[0]
+	except:
+		return index()
+	data = rq.get(
+		"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=C6BBD644E5F37D478E0AF48934CBD4EE&steamid={}&format=json".format(
+			id64))
+	try:
+		games_steam = json.loads(data.text)['response']['games']
+	except:
+		return index()
+	games_owned = []
+	game_a = {}
+	for g in games_steam:
+		time_played = (g['playtime_forever'] + 1) / 5.0
+		game = db.session.query(Game).filter(Game.steam_id == g['appid']).first()
+		if game is None: continue
+		k = np.max(costs[top_10k[game.name][0]])
+		the_id = np.where(costs[top_10k[game.name][0]] == k)
+
+		rez = []
+
+		for k in the_id[0]:
+			if len(rez) >= 5:
+				break
+			for x in games:
+				if top_10k[x][0] == k:
+					rez.append(x)
+		games_owned.append(game.name)
+		for x in rez:
+			if x in game_a:
+				game_a[x][0] += 1
+				game_a[x][1] += time_played
+			else:
+				game_a[x] = [1, time_played]
+	top_recs = sorted([(x, y[0] * y[1]) for x, y in game_a.items()], key=lambda x: x[1], reverse=True)
+	print(steam)
+	print(top_recs)
+	game_recs = set([x[0] for x in top_recs])
+	game_recs = list(game_recs - set(games_owned))
 
 	with app.test_request_context():
 		html_page = render_template("my_games.html", **{
-				'user': user,
-				'games': game_recs
+			'user': steam,
+			'games': game_recs[:10]
 		})
-
 		return html_page
+
+
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -141,7 +213,6 @@ if __name__ == '__main__':
 		text = text.replace('\n', '')
 		text = text.replace('  ', ' ')
 		lists = text[1:-1].split('] [')
-		print(len(lists))
 
 		lists[0] = lists[0][1:]
 		lists[-1] = lists[-1][:-1]
@@ -153,7 +224,12 @@ if __name__ == '__main__':
 	with open('top10k.txt') as f:
 		top_10k = json.load(f)
 	games = [x for x in top_10k.keys()]
-
+	# to_insert = []
+	# games_ids = {x[1]:y for y, x in top_10k.items()}
+	# for st_id, game in games_ids.items():
+	# 	to_insert.append(Game(name = game, steam_id  = st_id))
+	# db.session.bulk_save_objects(to_insert)
+	# db.session.commit()
 	costs = np.array(costs)
 	app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 	app.secret_key = os.urandom(12)
--- models.py
+++ models.py
@@ -1,27 +1,35 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, ForeignKey, Table
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
-class User(Base):
-	__tablename__ = 'users'
+association_table = Table('association', Base.metadata,
+						  Column('user_if', Integer, ForeignKey('User.id')),
+						  Column('game_id', Integer, ForeignKey('Game.id'))
+						  )
 
-	id = Column('users_id', Integer, primary_key=True)
+class User(Base):
+	__tablename__ = 'User'
+
+	id = Column(Integer, primary_key=True)
 	name = Column(String)
 	email = Column(String, unique=True)
-	games = relationship('Game', lazy='dynamic')
+	games = relationship('Game',
+						 secondary=association_table,
+						 back_populates="user")
 
 	def __repr__(self):
 		return "<User('%s','%s')>" % (self.name, self.email)
 
 class Game(Base):
-	__tablename__ = 'games'
+	__tablename__ = 'Game'
 
 	id = Column(Integer, primary_key=True)
 	name = Column(String, unique=True)
-	user_id = Column(Integer, ForeignKey('users.users_id'), nullable=False, onupdate='CASCADE')
-	user = relationship('User', backref='users')
+	steam_id = Column(Integer, unique=True)
+	user = relationship('User', secondary=association_table,
+						back_populates="games")
 
 	def __repr__(self):
-		return "<Game('%s','%s')>" % (self.name)
+		return "<Game('%s')>" % (self.name)
